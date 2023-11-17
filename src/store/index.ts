e12fae8fa7f3deca784eaf038db08d927d2d1cc4
carlosazeta <carlosagundezdev@gmail.com>
2023-11-17 13:17:22 +0100

    Use push to update state and toast for new user

--- src/store/index.ts
+++ src/store/index.ts
@@ -11,6 +11,9 @@ const syncWithDatabaseMiddleware: Middleware = store => next => action => {
   const { type, payload } = action
   const previousState = store.getState()
 
+  console.log("Action type:", type);
+
+
   if( type === 'users/deleteUserById') {
     const userToRemove = previousState.users.find(user => user.id === payload)
 
@@ -25,6 +28,29 @@ const syncWithDatabaseMiddleware: Middleware = store => next => action => {
       .catch(err => {
         toast.error(`Error deleting user ${payload}`)
         if (userToRemove) store.dispatch(rollbackUser(userToRemove))
+        console.log(err)
+      })
+  }
+
+  if(type === 'users/addNewUser') {
+    console.log('Entre aqui', type, payload)
+    fetch(`https://jsonplaceholder.typicode.com/users/`, {
+      method: 'POST',
+      body: JSON.stringify({
+        name: payload.name,
+        email: payload.email
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      }
+    })
+      .then(res => {
+        if(res.ok) {
+          toast.success(`User ${payload} create correctly`)
+        }
+      })
+      .catch(err => {
+        toast.error(`Error creating user ${payload}`)
         console.log(err)
       })
   }
--- src/store/users/slice.ts
+++ src/store/users/slice.ts
@@ -61,7 +61,7 @@ export const usersSlice = createSlice({
   reducers: {
     addNewUser:  (state, action: PayloadAction<User>) => {
       const id = crypto.randomUUID()
-      return [...state, {id, ...action.payload}]
+      state.push({id, ...action.payload})
     },
     deleteUserById: (state, action: PayloadAction<UserId>) => {
       const id = action.payload
@@ -70,7 +70,7 @@ export const usersSlice = createSlice({
     rollbackUser: (state, action: PayloadAction<UserWithId>) => {
       const isUserAlreadyDefined = state.some(user => user.id === action.payload.id)
       if (!isUserAlreadyDefined) {
-        return [...state, action.payload]
+        state.push(action.payload)
       }
     }
   }
